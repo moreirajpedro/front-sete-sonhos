c8eb7787cef9221e01821f69eab1ac1ba2b26d54
João Pedro Moreira de Oliveira <omoreirajpedro@gmail.com>
2020-09-30 07:35:11 -0300

    Chamando a API serverless do nodemailer para o formulario de newsletter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 import './Footer.css';
 
 function Footer() {
+    const [email, setEmail] = useState('')
+
+    const formSubmit = (e) => {
+        e.preventDefault();
+
+        let data = email;
+
+        axios.post('/send-email', data)
+            .then(res => {
+                setEmail('');
+            })
+            .catch(() => {
+                console.log('Message not sent');
+            })
+    }
+
     return (
         <footer id='footer-section' className='section-separator'>
             <div className="mail-list-register">
-                <p>Quer ficar por dentro de todas as nossas promoções? Cadastre o seu melhor email abaixo:</p>
-                <form>
-                    <input type="text" className='section-item-item-separator' placeholder='Insira aqui o seu melhor email' />
-                    <button type='button' >Inscreva-se</button>
+                <label className='mail-input-label' htmlFor='mail-input'>Quer ficar por dentro de todas as nossas promoções? Cadastre o seu melhor email abaixo:</label>
+                <form onSubmit={(e) => formSubmit(e)}>
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        name='email'
+                        type="email"
+                        className='mail-input section-item-item-separator'
+                        placeholder='Insira aqui o seu melhor email'
+                        required
+                        value={email}
+                    />
+                    <button type='submit' >Inscreva-se</button>
                 </form>
             </div>
             <div className="bulleted-section-list section-break-separator">
